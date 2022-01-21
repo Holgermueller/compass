c316dd2a7cc8735b4bcd68817c62db119672f23b
Holger Mueller <9jholger@gmail.com>
2022-01-21 11:41:06 -0600

    got html to reload

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,29 @@ const reload = require("browser-sync").reload;
 const cleanCss = require("gulp-clean-css");
 const rename = require("gulp-rename");
 const uglify = require("gulp-uglify-es").default;
-const { series, on } = require("gulp");
+const { series, on, watch } = require("gulp");
 
 const sync = () => {
   bs.init({
+    watch: true,
     server: {
       baseDir: "./",
     },
   });
 };
 
-const watch = () => {
-  gulp.watch("./").on("change", reload);
+const lessen = () => {
+  gulp.src("./css/*.less").pipe(less()).pipe(gulp.dest("./css"));
 };
 
-exports.default = series(sync, watch);
+const minify = () => {
+  gulp.src("/css/*.css").pipe(cleanCss()).pipe(gulp.dest("./css"));
+};
+
+const watchTasks = () => {
+  gulp.watch("./").on("change", reload);
+  gulp.watch("./css/*.less", series(lessen));
+  gulp.watch("./css/*.css", series(minify));
+};
+
+exports.default = series(sync, lessen, watchTasks);
