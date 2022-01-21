3a1bc910db9770c82f66430bfa8827ed727efe9f
Holger Mueller <9jholger@gmail.com>
2022-01-20 21:53:40 -0600

    got gulp running

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,22 @@
 const gulp = require("gulp");
 const less = require("less");
 const bs = require("browser-sync").create();
+const reload = require("browser-sync").reload;
 const cleanCss = require("gulp-clean-css");
 const rename = require("gulp-rename");
 const uglify = require("gulp-uglify-es").default;
+const { series, on } = require("gulp");
 
-gulp.task("browser-sync", () => {
+const sync = () => {
   bs.init({
     server: {
-      baseDir: "/",
+      baseDir: "./",
     },
   });
-});
+};
 
-gulp.task("default", ["browser-sync", "watch"]);
+const watch = () => {
+  gulp.watch("./").on("change", reload);
+};
+
+exports.default = series(sync, watch);
